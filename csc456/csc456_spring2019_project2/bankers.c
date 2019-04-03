09089b4124010fd002d855219b3abf1716842d97
HowCatIfDog <drach.spencer@gmail.com>
2019-04-02 23:52:00 -0600

    compiles and outputs stuff

--- csc456/csc456_spring2019_project2/bankers.c
+++ csc456/csc456_spring2019_project2/bankers.c
@@ -2,12 +2,15 @@
 #include "customer.h"
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 
 pthread_mutex_t pidMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex;
+pthread_mutex_t test_mutex;
 
 int main(int argc, char *argv[])
 {
-    if (argc != NUMBER_OF_RESOURCES)
+    if (argc != NUMBER_OF_RESOURCES+1)
     {
 	     printf("Incorrect number of arguments\n");
          return -1;
@@ -15,12 +18,14 @@ int main(int argc, char *argv[])
 
     for(int i = 0; i<NUMBER_OF_RESOURCES; i++)
     {
-        available[i] = argv[i];
+        // available[i] = strtol(argv[i+1]);
+        available[i] = atoi(argv[i+1]);
+        //available[i] = i+1;
     }
 
     initialize_arrays(maximum, allocation, need);
 
-
+    print_table(need, allocation, maximum, available);
 
 
 
@@ -38,7 +43,7 @@ int initialize_arrays(int maximum[][NUMBER_OF_RESOURCES],
     {
         for(j = 0; j<NUMBER_OF_RESOURCES; j++)
         {
-            maximum[i][j] = 1;
+            maximum[i][j] = 2;
         }
     }
 
@@ -57,30 +62,30 @@ int initialize_arrays(int maximum[][NUMBER_OF_RESOURCES],
 int request_resources(int customer_num, int request[])
 {
     /* acquire the mutex lock and warn if unable */
-    if(pthread_mutex_trylock(&pidMutex) != 0)
-    {
-        pthread_mutex_lock(&pidMutex);
-    }
-
-
-
-    /* release and warn if the mutex was not released  */
-    pthread_mutex_unlock(&pidMutex);
+    // if(pthread_mutex_trylock(&pidMutex) != 0)
+    // {
+    //     pthread_mutex_lock(&pidMutex);
+    // }
+    //
+    //
+    //
+    // /* release and warn if the mutex was not released  */
+    // pthread_mutex_unlock(&pidMutex);
     return 0;
 }
 
 int release_resources(int customer_num, int release[])
 {
     /* acquire the mutex lock and warn if unable */
-    if(pthread_mutex_trylock(&pidMutex) != 0)
-    {
-        pthread_mutex_lock(&pidMutex);
-    }
-
-
-
-    /* release and warn if the mutex was not released  */
-    pthread_mutex_unlock(&pidMutex);
+    // if(pthread_mutex_trylock(&pidMutex) != 0)
+    // {
+    //     pthread_mutex_lock(&pidMutex);
+    // }
+    //
+    //
+    //
+    // /* release and warn if the mutex was not released  */
+    // pthread_mutex_unlock(&pidMutex);
     return 0;
 }
 
@@ -97,7 +102,9 @@ int update_need(int need[][NUMBER_OF_RESOURCES],
     {
         for(int j = 0; j<NUMBER_OF_RESOURCES; j++)
         {
-            need[i][j] = maximum[i][j] - allocation[i][j];
+            //no idea why but there needs to be a negative there
+            need[i][j] = -(maximum[i][j] - allocation[i][j]);
+            //printf("%d \n", maximum[i][j]-allocation[i][j]);
         }
     }
 
@@ -109,7 +116,7 @@ void print_table(int need[][NUMBER_OF_RESOURCES],
     int maximum[][NUMBER_OF_RESOURCES],
     int available[])
 {
-    printf("     Allocation    Need     Available\n");
+    printf("     Allocation    Need     Maximum     Available\n");
     for(int i = 0; i<NUMBER_OF_CUSTOMERS; i++)
     {
         printf("P%d     ", i);
@@ -125,17 +132,19 @@ void print_table(int need[][NUMBER_OF_RESOURCES],
         printf("     ");
         for(int l = 0; l<NUMBER_OF_RESOURCES; l++)
         {
-            printf("%d ", need[i][l]);
+            printf("%d ", maximum[i][l]);
         }
+        if(i == 0)
+        {
+          printf("     ");
+          for(int m = 0; m < NUMBER_OF_RESOURCES; m++)
+          {
+            printf("%d ", available[m]);
+          }
+        }
+        printf("\n");
     }
-    // printf("P0     1 1 1      1 1 1       1 1 1  \n");
-    // printf("P1     1 1 1      1 1 1       1 1 1  \n");
-    // printf("P2     1 1 1      1 1 1       1 1 1  \n");
-    // printf("P3     1 1 1      1 1 1       1 1 1  \n");
-    // printf("P4     1 1 1      1 1 1       1 1 1  \n");
-    // printf("P5     1 1 1      1 1 1       1 1 1  \n");
-    // printf("P6     1 1 1      1 1 1       1 1 1  \n");
-    // printf("P7     1 1 1      1 1 1       1 1 1  \n");
+
 
     return;
 }
